7be946c89f87ce00b5995d30ab1d82162ec6162f
MarciaOrr <orrmar@swcp.com>
2017-02-02 23:55:20 +0000

    fixed production mailer for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  cofig.action_mailer.default_url_options = { host: 'grammable-marcia-orr.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'grammable-marcia-orr.herokuapp.com' }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
